e9388ddb8691ec105a44c39584951565921980e7
FL33TW00D <fleetwoodpersonal@gmail.com>
2023-03-15 21:23:15 +0000

    chore: FUCK S3

--- src/s3/s3Client.ts
+++ src/s3/s3Client.ts
@@ -1,14 +1,11 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
-import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
-import { IDENTITY_POOL_ID, REGION } from "../config.json";
 
 const s3Client = new S3Client({
-  region: REGION,
-  credentials: fromCognitoIdentityPool({
-    client: new CognitoIdentityClient({ region: REGION }),
-    identityPoolId: IDENTITY_POOL_ID,
-  }),
+  region: "auto",
+  credentials: {
+    accessKeyId: "AKIAIOSFODNN7EXAMPLE",
+    secretAccessKey: "bingabonga",
+  }
 });
 
 export { s3Client };
