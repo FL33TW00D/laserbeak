11a650801928b73c68463d812fdcd2aa484b11ca
FL33TW00D <fleetwoodpersonal@gmail.com>
2023-04-11 17:16:54 +0100

    chore: editor working good

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,27 +1,27 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import HoveringMenuExample from "../components/editor";
 import { FLANExample } from "../components/FLANExample";
 
 const Home: NextPage = () => {
     return (
         <div className="p-0">
             <Head>
-                <title>Talk to FLAN-T5</title>
-                <meta name="description" content="Talk to FLAN-T5" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+                <title>Summize</title>
+                <meta name="description" content="AI Text Editor" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className="min-h-screen flex flex-1 flex-col bg-dark">
-                <div className="max-w-5xl m-auto p-16 flex-col justify-center content-center align-center">
-                    <h1 className="text-4xl md:text-6xl font-medium tracking-tight text-center mb-16 text-white ">
-                        Welcome to LLMs in the browser!
+                    <h1 className="text-xl md:text-2xl font-medium tracking-tight text-white pl-4 py-4">
+                        Summize
                     </h1>
-
-                    <div className="text-center">
-                        <FLANExample />
+                <div className="mx-auto flex flex-1 flex-col justify-center content-center align-center h-full w-full">
+                    <div className="text-center bg-white flex flex-1 p-4 w-full">
+                        <HoveringMenuExample />
                     </div>
                 </div>
-
                 <div className="flex flex-col text-center">
                     <div className="flex h-full flex-col justify-between gap-y-4 pt-8 pb-6 text-sm md:col-span-1">
                         <span className="font-extralight text-white">
