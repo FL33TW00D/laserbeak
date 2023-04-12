64102e4cd56deb9933682d28d4da4fba8c533f81
FL33TW00D <fleetwoodpersonal@gmail.com>
2023-04-12 17:51:21 +0100

    chore: working

--- site/src/components/editor.tsx
+++ site/src/components/editor.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useEffect } from "react";
+import React, { useMemo, useRef, useEffect, useState, useContext } from "react";
 import { Slate, Editable, withReact, useSlate, useFocused } from "slate-react";
 import {
     Editor,
@@ -12,47 +12,100 @@ import { css } from "@emotion/css";
 import { withHistory } from "slate-history";
 
 import { Button, Icon, Menu, Portal } from "./components";
+import { useMountEffectOnce } from "../hooks/useMountEffectOnce";
+import { ModelManager, AvailableModels } from "laserbeak";
+
+const modelContext = React.createContext<any | null>(null);
 
 const HoveringMenuExample = () => {
     const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+    const [model, setModel] = useState<any | null>(null);
+
+    useMountEffectOnce(() => {
+        (async () => {
+            let modelManager = new ModelManager();
+            await modelManager.init();
+            let loadedModel = await modelManager.loadModel(
+                AvailableModels.FLAN_T5_BASE
+            );
+            setModel(loadedModel);
+        })();
+    });
 
     return (
         <Slate editor={editor} value={initialValue}>
-            <HoveringToolbar />
-            <Editable
-                renderLeaf={(props) => <Leaf {...props} />}
-                placeholder="Enter some text..."
-                onDOMBeforeInput={(event: InputEvent) => {
-                    switch (event.inputType) {
-                        case "formatBold":
-                            event.preventDefault();
-                            return toggleFormat(editor, "bold");
-                        case "formatItalic":
-                            event.preventDefault();
-                            return toggleFormat(editor, "italic");
-                        case "formatunderline":
-                            event.preventdefault();
-                            return toggleformat(editor, "underlined");
-                        case "summarize":
-                            event.preventDefault();
-                            return handleSummarize(editor);
-                            
-                    }
-                }}
-                style={{
-                    minWidth: "100%",
-                }}
-            />
+            <modelContext.Provider value={model}>
+                <HoveringToolbar />
+                <Editable
+                    renderLeaf={(props) => <Leaf {...props} />}
+                    placeholder="Enter some text..."
+                    onDOMBeforeInput={(event: InputEvent) => {
+                        switch (event.inputType) {
+                            case "formatBold":
+                                event.preventDefault();
+                                return toggleFormat(editor, "bold");
+                            case "formatItalic":
+                                event.preventDefault();
+                                return toggleFormat(editor, "italic");
+                            case "formatunderline":
+                                event.preventDefault();
+                                return toggleFormat(editor, "underlined");
+                            case "summarize":
+                                event.preventDefault();
+                                return handleSummarize(model, editor);
+                        }
+                    }}
+                    style={{
+                        minWidth: "100%",
+                    }}
+                />
+            </modelContext.Provider>
         </Slate>
     );
 };
 
-const handleSummarize = (editor) => {
+async function runSample(
+    model: any,
+    editor: Editor,
+    inputText: string,
+    selection: Selection
+) {
+    try {
+        if (!model || !inputText || inputText.length < 2) {
+            return;
+        }
+        let start_location = selection.focus;
+        const start = performance.now();
+        let prevOutput = "";
+        await model.run(inputText, (output: string) => {
+            Transforms.insertText(editor, output.substring(prevOutput.length), {
+                at: {
+                    path: start_location.path,
+                    offset: start_location.offset + prevOutput.length,
+                },
+            });
+            prevOutput = output;
+        });
+        const duration = performance.now() - start;
+        console.log("Inference time:", duration.toFixed(2), "ms");
+    } catch (e: any) {
+        console.log(e.toString());
+    }
+}
+
+const handleSummarize = (model: any, editor: Editor) => {
+    console.log("Calling summarize");
+    console.log("Selection: ", editor.selection);
+    if (!editor.selection) {
+        return;
+    }
+    let input_selection = Editor.string(editor, editor.selection);
+    let input_text = `Summarize:\n\n${input_selection}`;
     Transforms.delete(editor, { at: editor.selection });
-    Transforms.insertText(editor, "Summarized text");
+    runSample(model, editor, input_text, editor.selection);
 };
 
-const toggleFormat = (editor, format) => {
+const toggleFormat = (editor: Editor, format) => {
     const isActive = isFormatActive(editor, format);
     Transforms.setNodes(
         editor,
@@ -61,7 +114,7 @@ const toggleFormat = (editor, format) => {
     );
 };
 
-const isFormatActive = (editor, format) => {
+const isFormatActive = (editor: Editor, format) => {
     const [match] = Editor.nodes(editor, {
         match: (n) => n[format] === true,
         mode: "all",
@@ -164,11 +217,9 @@ const FormatButton = ({ format, icon }) => {
 
 const SummarizeButton = () => {
     const editor = useSlate();
+    const model = useContext(modelContext);
     return (
-        <Button
-            reversed
-            onClick={() => handleSummarize(editor)}
-        >
+        <Button reversed onClick={() => handleSummarize(model, editor)}>
             <Icon>{"summarize"}</Icon>
         </Button>
     );
@@ -178,24 +229,31 @@ const initialValue: Descendant[] = [
     {
         type: "paragraph",
         children: [
-            {
-                text: "This example shows how you can make a hovering menu appear above your content, which you can use to make text ",
-            },
-            { text: "bold", bold: true },
-            { text: ", " },
-            { text: "italic", italic: true },
-            { text: ", or anything else you might want to do!" },
+            { text: "This is editable " },
+            { text: "rich", bold: true },
+            { text: " text, " },
+            { text: "much", italic: true },
+            { text: " better than a " },
+            { text: "!" },
         ],
     },
     {
         type: "paragraph",
         children: [
-            { text: "Try it out yourself! Just " },
             {
-                text: "select any piece of text and the menu will appear",
-                bold: true,
+                text: "This document reflects the strategy we’ve refined over the past two years, including feedback from many people internal and external to OpenAI. The timeline to AGI remains uncertain, but our Charter will guide us in acting in the best interests of humanity throughout its development. OpenAI’s mission is to ensure that artificial general intelligence (AGI)—by which we mean highly autonomous systems that outperform humans at most economically valuable work—benefits all of humanity. We will attempt to directly build safe and beneficial AGI, but will also consider our mission fulfilled if our work aids others to achieve this outcome. To that end, we commit to the following principles\n Broadly distributed benefits\n We commit to use any influence we obtain over AGI’s deployment to ensure it is used for the benefit of all, and to avoid enabling uses of AI or AGI that harm humanity or unduly concentrate power.Our primary fiduciary duty is to humanity. We anticipate needing to marshal substantial resources to fulfill our mission, but will always diligently act to minimize conflicts of interest among our employees and stakeholders that could compromise broad benefit.\nLong-term safety\nWe are committed to doing the research required to make AGI safe, and to driving the broad adoption of such research across the AI community. We are concerned about late-stage AGI development becoming a competitive race without time for adequate safety precautions. Therefore, if a value-aligned, safety-conscious project comes close to building AGI before we do, we commit to stop competing with and start assisting this project. We will work out specifics in case-by-case agreements, but a typical triggering condition might be “a better-than-even chance of success in the next two years.”\n Technical Leadership \n To be effective at addressing AGI’s impact on society, OpenAI must be on the cutting edge of AI capabilities—policy and safety advocacy alone would be insufficient. We believe that AI will have broad societal impact before AGI, and we’ll strive to lead in those areas that are directly aligned with our mission and expertise. \n Cooperative Coordination \n We will actively cooperate with other research and policy institutions; we seek to create a global community working together to address AGI’s global challenges.We are committed to providing public goods that help society navigate the path to AGI. Today this includes publishing most of our AI research, but we expect that safety and security concerns will reduce our traditional publishing in the future, while increasing the importance of sharing safety, policy, and standards research.",
             },
-            { text: "." },
+        ],
+    },
+    {
+        type: "paragraph",
+        children: [
+            { text: "This is editable " },
+            { text: "rich", bold: true },
+            { text: " text, " },
+            { text: "much", italic: true },
+            { text: " better than a " },
+            { text: "!" },
         ],
     },
 ];
--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,31 +1,38 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import HoveringMenuExample from "../components/editor";
-import { FLANExample } from "../components/FLANExample";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const Home: NextPage = () => {
     return (
-        <div className="p-0">
+        <div className={`p-0 ${inter.className}`}>
             <Head>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
                 <title>Summize</title>
                 <meta name="description" content="AI Text Editor" />
                 <link rel="icon" href="/favicon.ico" />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                ></link>
             </Head>
 
             <main className="min-h-screen flex flex-1 flex-col bg-dark">
-                    <h1 className="text-xl md:text-2xl font-medium tracking-tight text-white pl-4 py-4">
-                        Summize
-                    </h1>
+                <h1 className="text-xl md:text-2xl font-medium tracking-tight text-white pl-4 py-4">
+                    Summize
+                </h1>
                 <div className="mx-auto flex flex-1 flex-col justify-center content-center align-center h-full w-full">
-                    <div className="text-center bg-white flex flex-1 p-4 w-full">
-                        <HoveringMenuExample />
+                    <div className="text-center bg-white flex flex-1 px-4 py-8">
+                        <div className="flex flex-1 max-w-5xl mx-auto">
+                            <HoveringMenuExample />
+                        </div>
                     </div>
                 </div>
                 <div className="flex flex-col text-center">
-                    <div className="flex h-full flex-col justify-between gap-y-4 pt-8 pb-6 text-sm md:col-span-1">
+                    <div className="flex h-full flex-col justify-between gap-y-4 pt-4 pb-6 text-sm md:col-span-1">
                         <span className="font-extralight text-white">
-                            Devised by{" "}
+                            Built by {" "}
                             <a
                                 className="font-light text-blue-500 hover:text-blue-700"
                                 href="https://fleetwood.dev"
@@ -33,6 +40,16 @@ const Home: NextPage = () => {
                                 Christopher Fleetwood
                             </a>
                         </span>
+                        <span className="font-extralight text-white">
+                            Learn more {" "} 
+                            <a
+                                className="font-light text-blue-500 hover:text-blue-700"
+                                href="https://fleetwood.dev"
+                            >
+                                here
+                            </a>
+                        </span>
+
                     </div>
                 </div>
             </main>
